2ca1112b51acad140e71e94c5f4b378bb58cf372
Rebelpebble <neasbrian@gmail.com>
2017-12-28 16:35:59 -0600

    Add solution for problem 4

---
+++ 4/index.js
@@ -0,0 +1,30 @@
+function solution(start, end) {
+  const allPalindromeNumbers = []
+
+  for (let i = start; i <= end; i++) {
+    for (let j = end; j >= start; j--) {
+      if (checkIfPalindrome(i * j)) {
+        allPalindromeNumbers.push([i, j, i * j])
+      }
+    }
+  }
+
+
+  allPalindromeNumbers.sort( (a, b) => {
+    const productA = a[2]
+    const productB = b[2]
+
+    return productA - productB
+  })
+
+  return allPalindromeNumbers.pop()
+}
+
+function checkIfPalindrome(number) {
+  const digits = String(number)
+  const digitsReversed = digits.split('').reverse().join('')
+
+  return digits === digitsReversed
+}
+
+console.log(solution(1000, 9999))
